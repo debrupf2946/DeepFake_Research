a8f1a626282a0fc213f80da2892a93d62a38fe58
debrupf2946 <f20212946@goa.bits-pilani.ac.in>
2024-09-02 09:00:42 +0530

    added xception_GRU model

--- models.py
+++ models.py
@@ -1,2 +1,21 @@
 from classification_models.keras import Classifiers
 from tensorflow import keras
+
+def get_models(model: str):
+    SIZE = 299
+    DIM = (SIZE, SIZE, 3)
+    BATCH_SIZE = 150
+    if model=="xception_GRU":
+        
+        n_classes = 1
+        net, preprocess_input = Classifiers.get('xception')
+        base_model = net(input_shape=DIM, weights='imagenet', include_top=False)
+        x = keras.layers.GlobalAveragePooling2D()(base_model.output)
+        x = keras.layers.Reshape((1, -1))(x)
+        x = GRU(64)(x)
+        output = keras.layers.Dense(n_classes, activation='sigmoid')(x)
+        model = keras.models.Model(inputs=[base_model.input], outputs=[output])
+return model
+        
+         
+    
